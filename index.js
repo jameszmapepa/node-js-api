99cc95ce80d4a5acfe9f7eeb47c68b4e33e3222f
james.mapepa <james.mapepa@cassavasmartech.co.zw>
2020-08-11 08:38:40 +0200

    Add books resource

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
+let books = require("./books.json");
+
 const app = express();
 
-app.get('/', async(req, res) => {
+app.get("/", async(req, res) => {
   res.send("Hello nyika");
+});
+
+app.get("/books", async(req, res) => {
+  res.json(books.books);
 });
 
 const port = 3000;
